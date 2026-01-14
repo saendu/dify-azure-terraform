f269ac384af2fe7a8a9235e6e3b758f915e7e08e
Sandro <sandrek@gmail.com>
2026-01-14 14:22:15 +0100

    add internal load balancer

--- aca-env.tf
+++ aca-env.tf
@@ -12,6 +12,7 @@ resource "azurerm_container_app_environment" "dify-aca-env" {
   location                   = azurerm_resource_group.rg.location
   resource_group_name        = azurerm_resource_group.rg.name
   log_analytics_workspace_id = azurerm_log_analytics_workspace.aca-loga.id
+  internal_load_balancer_enabled = true
   infrastructure_subnet_id = azurerm_subnet.acasubnet.id
   workload_profile  {
     name = "Consumption"
