69108d80ba03a506b9fcba126b027ddc21417aaa
Sandro <sandrek@gmail.com>
2026-01-14 14:36:35 +0100

    make pubic available again

--- redis-cache.tf
+++ redis-cache.tf
@@ -15,7 +15,7 @@ resource "azurerm_redis_cache" "redis" {
   enable_non_ssl_port = true
   minimum_tls_version = "1.2"
 
-  public_network_access_enabled = false
+  public_network_access_enabled = true
   redis_version       = "6"
   
   # subnet_id           = azurerm_subnet.redissubnet.id
@@ -26,38 +26,6 @@ resource "azurerm_redis_cache" "redis" {
   }
 }
 
-resource "azurerm_private_endpoint" "pe-redis" {
-  name                = "pe-redis"
-  location            = azurerm_resource_group.rg.location
-  resource_group_name = azurerm_resource_group.rg.name
-  subnet_id           = azurerm_subnet.privatelinksubnet.id
-
-  private_service_connection {
-    name                           = "psc-redis"
-    private_connection_resource_id = azurerm_redis_cache.redis.id
-    subresource_names              = ["redisCache"]
-    is_manual_connection           = false
-  }
-
-  private_dns_zone_group {
-    name                 = "pdz-stor"
-    private_dns_zone_ids = [azurerm_private_dns_zone.redis.id]
-  }
-}
-
-resource "azurerm_private_dns_zone" "redis" {
-  name                = "privatelink.redis.cache.windows.net"
-  resource_group_name = azurerm_resource_group.rg.name
-}
-
-resource "azurerm_private_dns_zone_virtual_network_link" "redis-vnet" {
-  name                  = "redis-dns-link"
-
-  resource_group_name   = azurerm_resource_group.rg.name
-  private_dns_zone_name = azurerm_private_dns_zone.redis.name
-  virtual_network_id    = azurerm_virtual_network.vnet.id
-}
-
 output "redis_cache_hostname" {
   value = azurerm_redis_cache.redis.hostname
 }
@@ -66,4 +34,3 @@ output "redis_cache_key" {
   value = azurerm_redis_cache.redis.primary_access_key
   sensitive = true
 }
-
